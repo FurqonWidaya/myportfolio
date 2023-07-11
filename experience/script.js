d76275624f2570e43eb96d9f5d339ba5f3fd889e
FurqonWidaya <55239325+FurqonWidaya@users.noreply.github.com>
2023-07-11 23:00:23 +0700

    ..

--- experience/script.js
+++ experience/script.js
@@ -63,13 +63,13 @@ document.onkeydown = function(e) {
 }
 
 document.addEventListener('visibilitychange',
-function(){
-    if(document.visibilityState === "visible"){
-        document.title = "Experience | Portfolio Jigar Sable";
-        $("#favicon").attr("href","/assets/images/favicon.png");
-    }
-    else {
-        document.title = "Come Back To Portfolio";
-        $("#favicon").attr("href","/assets/images/favhand.png");
-    }
-});
+    function () {
+        if (document.visibilityState === "visible") {
+            document.title = "My Portfolio | Furqon Aji Widaya";
+            $("#favicon").attr("href", "assets/images/favicon.png");
+        }
+        else {
+            // document.title = "Come Back To Portfolio";
+            // $("#favicon").attr("href", "assets/images/favhand.png");
+        }
+    });
